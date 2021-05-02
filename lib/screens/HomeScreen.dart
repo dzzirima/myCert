b134a564401e4eca47410e5cfc335044f75eb1b5
David Zirima <davidtzirima@gmail.com>
2021-05-02 22:18:48 +0200

    added the  floating button

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -9,7 +9,55 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(" Home"),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child:CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 53.0,
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/david.jpeg'),
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+              Expanded(
+                flex: 2,
+                child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    color: Colors.amber[600],
+                    child: const Center(child: Text('Entry A')),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[500],
+                    child: const Center(child: Text('Entry B')),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[100],
+                    child: const Center(child: Text('Entry C')),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {// Add your onPressed code here!
+            print("The floating action buton was pressed 22");
+          },
+        child: Icon(Icons.add),
+
       ),
     );
   }
