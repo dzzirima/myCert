588ddc226d87635dc599b915040c4b4569c007ae
Entwickler_Sherr <62500909+EntwicklerSherr@users.noreply.github.com>
2021-05-13 19:27:11 +0200

    Update Alert Screens
    1. Alert screens is now the Shared page
    2. I Didn't change file name.
    3. Created pop up menus
    4. List of Documents are not real documents but texts with pdf icon for purposes of demo day

--- lib/screens/AlertsScreen.dart
+++ lib/screens/AlertsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_cert/screens/revokedpage.dart';
 
 class Alerts extends StatefulWidget {
   @override
@@ -8,11 +9,105 @@ class Alerts extends StatefulWidget {
 class _AlertsState extends State<Alerts> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Alerts"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.share
+                  //color: Colors.green[500],
+                  ),
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Shared Certificates'),
+              ),
+            ],
+          ),
+          centerTitle: true,
+          actions: [
+            PopupMenuButton<int>(
+                onSelected: (item) => onSelected(context, item),
+                itemBuilder: (context) => [
+                      PopupMenuItem(
+                        value: 0,
+                        child: Text('Revoke All'),
+                      ),
+                    ])
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 50,
+              child: Row(
+                
+                children: [
+                  
+                  Icon(Icons.picture_as_pdf),
+                  Text('Transcript.pdf'),
+                  Positioned(child: 
+                  PopupMenuButton<int>(
+                    
+                      onSelected: (item) => onSelected(context, item),
+                      itemBuilder: (context) => [
+                            PopupMenuItem(
+                              value: 0,
+                              child: Text('Revoke'),
+                            ),
+                          ])
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 50,
+              child: Row(
+                children: [
+                  Icon(Icons.picture_as_pdf),
+                  Text('Advanced Flutter Certficate.pdf'),
+                  PopupMenuButton<int>(
+                      onSelected: (item) => onSelected(context, item),
+                      itemBuilder: (context) => [
+                            PopupMenuItem(
+                              value: 0,
+                              child: Text('Revoke'),
+                            ),
+                          ])
+                ],
+              ),
+            ),
+            Container(
+              height: 50,
+              child: Row(
+                children: [
+                  Icon(Icons.picture_as_pdf),
+                  Text('David Zirima Python Basics.pdf'),
+                  PopupMenuButton<int>(
+                      onSelected: (item) => onSelected(context, item),
+                      itemBuilder: (context) => [
+                            PopupMenuItem(
+                              value: 0,
+                              child: Text('Revoke'),
+                            ),
+                          ])
+                ],
+              ),
+            ),
+          ],
+          //Center(
+          //  child: Text("Shared"),
+          //),
+        ),
       ),
     );
   }
+
+  void onSelected(BuildContext context, int item) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => RevokedPage()),
+    );
+  }
 }
+
 
