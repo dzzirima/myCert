33ba2e8a84fc27880c83a0d90d67c74ada63a532
David Zirima <davidtzirima@gmail.com>
2021-05-13 23:12:13 +0200

    making changes to files

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 
 class Profile extends StatefulWidget {
@@ -10,9 +12,120 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(" profile"),
-      ),
+
+        body: Container(
+          padding: EdgeInsets.only(left: 10, top: 10,right: 15),
+          child: GestureDetector(
+            onTap:(){
+              FocusScope.of(context).unfocus();
+            } ,
+            child: ListView(
+              children: [
+                Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 130,
+                        height: 120,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 4, color: Colors.white),
+                            boxShadow: [
+                              BoxShadow(
+                                  spreadRadius:2,
+                                  blurRadius: 3,
+                                  color: Colors.black.withOpacity(0.1)
+                              )
+                            ],
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage('https://cdn.pixabay.com/photo/2015/03/03/18/58/woman-657753_960_720.jpg')
+                            )
+                        ),
+                      ),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  width: 4,
+                                  color: Colors.white,
+                                ),
+                                color: Colors.blue
+                            ),
+                            child: Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                          )
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30,),
+                buildTextField("Full Name", "Nicola Dyakonda"),
+                buildTextField("Email address", "email@gmail.com"),
+                buildTextField("Phone number", "123456789"),
+                buildTextField("Bio", "small infor about yourself"),
+                buildTextField("Work Experience", null),
+                buildTextField("  Education", null),
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    OutlinedButton(onPressed:(){},
+                      child: Text("Edit", style: TextStyle(
+                        fontSize:15,
+                        letterSpacing: 5,
+                        color: Colors.black,
+                      ),),
+                      style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(horizontal: 30),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+                      ),
+                    ),
+                    ElevatedButton(onPressed:() {},
+                      child:Text("SAVE",style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.white
+                      ),),
+                      style:ElevatedButton.styleFrom(
+                          primary: Colors.blue,
+                          padding: EdgeInsets.symmetric(horizontal: 50,),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                      ) ,)
+                  ],)
+              ],
+            ),
+          ),
+        )
     );
   }
+  Widget buildTextField(String labelText, String placeholder){
+    return Padding(padding:EdgeInsets.only(bottom: 30),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(bottom: 5),
+          labelText: labelText,
+          labelStyle: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black),
+
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+            color: Colors.black54,
+          ),
+        ),
+      ),);
+  }
+
 }
