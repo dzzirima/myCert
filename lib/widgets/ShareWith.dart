0c8b00eb5160d8f257b454dd75ec67a1ad9dea4b
dzzirima@gmail.com <dzzirima@gmail.com>
2021-07-13 23:11:29 +0200

    working with dynamic list

--- lib/widgets/ShareWith.dart
+++ lib/widgets/ShareWith.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SharedWith extends StatefulWidget {
   const SharedWith({Key key}) : super(key: key);
@@ -8,6 +9,21 @@ class SharedWith extends StatefulWidget {
 }
 
 class _SharedWithState extends State<SharedWith> {
+  //  this is where you add all the state variable
+  final fields = [];
+  void _addField(){
+    fields.add('');
+    setState(() {
+    });
+  }
+
+  void _removeField(index){
+    fields.removeAt(index);
+    setState(() {
+    });
+
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,8 +31,23 @@ class _SharedWithState extends State<SharedWith> {
         Text("Share with",
         style: CupertinoTheme.of(context).textTheme.navActionTextStyle,),
         SizedBox(height: 15.0),
+        for(var i = 0; i < fields.length ; i++)
+        Row(
+          children: [
+            Expanded(child: CupertinoTextField(
+              //padding: const EdgeInsets.all(18.0),
+            ),
+            ),
+            CupertinoButton(child:
+                Icon(CupertinoIcons.minus_circle),
+                onPressed: (){
+                _removeField(i);
+                })
+          ],
+        ),
         CupertinoButton(child: Text("+ Add @sign"), onPressed: (){
           //TODO add field
+          _addField();
         })
       ],
     );
